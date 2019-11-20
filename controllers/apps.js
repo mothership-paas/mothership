dd9ced2f829b44203e1dbb50063112d0edc9b052
jkulton <jonkulton@gmail.com>
2019-11-19 20:16:30 -0500

    Create app on Swarm rather than lone droplet
    Co-Authored-By: Ian Evans <icevans@icloud.com>
    Co-Authored-By: Bharat <bharatagarwal@gmail.com>

--- controllers/apps.js
+++ controllers/apps.js
@@ -1,4 +1,5 @@
 const App = require('../server/models').App;
+const Node = require('../server/models').Node;
 const Docker = require('dockerode');
 const Machine = require('docker-machine');
 const cmd = process.argv.slice(2);
@@ -23,21 +24,6 @@ EXPOSE 4567
 CMD ["bundle", "exec", "puma", "-t", "5:5", "-p", "4567"]`;
 };
 
-const buildDroplet = (app) => {
-  return new Promise((resolve, reject) => {
-    console.log('Building droplet...');
-
-    const options = {
-      'digitalocean-access-token': process.env.ACCESS_TOKEN,
-    };
-
-    Machine.create(app.dropletName, 'digitalocean', options, (err) => {
-      if (err) throw err;
-      resolve(app);
-    });
-  });
-};
-
 const buildDockerfile = (file) => {
   return (app) => {
     return new Promise((resolve, reject) => {
@@ -48,63 +34,136 @@ const buildDockerfile = (file) => {
   };
 };
 
-const buildAndRunContainer = (app) => {
+const createService = app => {
+  return new Promise((resolve, reject) => {
+    Node.findOne({ where: { manager: true }}).then((manager) => {
+      const machine = new Machine(manager.name);
+
+      machine.env({ parse: true }, (err, result) => {
+        if (err) { console.log(err); }
+
+        const certPath = result.DOCKER_CERT_PATH;
+        const hostWithPort = result.DOCKER_HOST.split('//')[1];
+        const host = hostWithPort.split(':')[0];
+        const port = hostWithPort.split(':')[1];
+
+        const options = {
+          socketPath: undefined,
+          host: host,
+          port: Number(port),
+          ca: fs.readFileSync(certPath + '/ca.pem'),
+          cert: fs.readFileSync(certPath + '/cert.pem'),
+          key: fs.readFileSync(certPath + '/key.pem'),
+        };
+
+        const docker = new Docker(options);
+
+        const serviceParams = {
+          "Name": "web",
+          "TaskTemplate": {
+            "ContainerSpec": {
+              "Image": `${app.title}:latest`,
+            },
+            "Networks": [
+              { "Target": `${app.title}_default` }
+            ]
+          },
+          "EndpointSpec": {
+            "Ports": [
+              {
+                "Protocol": "tcp",
+                "PublishedPort": 8080,
+                "TargetPort": 4567
+              }
+            ]
+          },
+        };
+
+        docker.createService(serviceParams, (error, result) => {
+          resolve(app);
+        });
+      });
+    });
+  });
+};
+
+const buildImage = (app) => {
   return new Promise(async (resolve, reject) => {
-    console.log('Building + running container on droplet...');
-    const machine = new Machine(app.dropletName);
+    Node.findOne({ where: { manager: true }}).then((manager) => {
+      console.log('Building + running container on droplet...');
+      const machine = new Machine(manager.name);
 
-    machine.env({ parse: true }, (err, result) => {
-      const certPath = result.DOCKER_CERT_PATH;
-      const hostWithPort = result.DOCKER_HOST.split('//')[1];
-      const host = hostWithPort.split(':')[0];
-      const port = hostWithPort.split(':')[1];
+      machine.env({ parse: true }, (err, result) => {
+        if (err) { console.log(err); }
+        const certPath = result.DOCKER_CERT_PATH;
+        const hostWithPort = result.DOCKER_HOST.split('//')[1];
+        const host = hostWithPort.split(':')[0];
+        const port = hostWithPort.split(':')[1];
 
-      const options = {
-        socketPath: undefined,
-        host: host,
-        port: Number(port),
-        ca: fs.readFileSync(certPath + '/ca.pem'),
-        cert: fs.readFileSync(certPath + '/cert.pem'),
-        key: fs.readFileSync(certPath + '/key.pem'),
-      };
+        const options = {
+          socketPath: undefined,
+          host: host,
+          port: Number(port),
+          ca: fs.readFileSync(certPath + '/ca.pem'),
+          cert: fs.readFileSync(certPath + '/cert.pem'),
+          key: fs.readFileSync(certPath + '/key.pem'),
+        };
 
-      const docker = new Docker(options);
+        const docker = new Docker(options);
+        const tarStream = tar.pack(app.path);
 
-      var tarStream = tar.pack(app.path);
-      docker.buildImage(tarStream, {
-        t: `${app.title}:latest`,
-      }, function(error, output) {
-        if (error) {
-          return console.error(error);
-        }
-        output.pipe(process.stdout);
-        output.on('end', function () {
-          // send notification to client that build is ready
-          var container = docker.createContainer({
-            Image: app.title + ':latest',
-            PortBindings: { "4567/tcp": [{ HostPort: "80" }] }
-          }).then(container => {
-            return container.start();
-          }).then(container => {
+        docker.buildImage(tarStream, {
+          t: `${app.title}:latest`,
+        }, function(error, output) {
+          if (error) {
+            return console.error(error);
+          }
+          output.pipe(process.stdout);
+          output.on('end', function () {
+            // send notification to client that build is ready
             resolve(app);
-          }).catch(error => {
-            throw error;
           });
         });
       });
     });
   });
-}
+};
 
-const saveIpAddress = (app) => {
-  return new Promise((resolve, reject) => {
-    const machine = new Machine(app.dropletName);
+const createNetwork = (app) => {
+  return new Promise(async (resolve, reject) => {
+    Node.findOne({ where: { manager: true }}).then((manager) => {
+      console.log('Creating network...');
+      const machine = new Machine(manager.name);
 
-    machine.inspect((err, result) => {
-      if (err) throw err;
+      machine.env({ parse: true }, (err, result) => {
+        console.log(err);
+        const certPath = result.DOCKER_CERT_PATH;
+        const hostWithPort = result.DOCKER_HOST.split('//')[1];
+        const host = hostWithPort.split(':')[0];
+        const port = hostWithPort.split(':')[1];
 
-      const ipAddress = result.driver.ipAddress;
-      app.update({ ipAddress }).then((app) => resolve(app));
+        const options = {
+          socketPath: undefined,
+          host: host,
+          port: Number(port),
+          ca: fs.readFileSync(certPath + '/ca.pem'),
+          cert: fs.readFileSync(certPath + '/cert.pem'),
+          key: fs.readFileSync(certPath + '/key.pem'),
+        };
+
+        const docker = new Docker(options);
+
+        const networkOptions = {
+          "Name": app.title + '_default',
+          "Driver": "overlay"
+        };
+
+        docker.createNetwork(networkOptions, (error, result) => {
+          console.log(error);
+          console.log(result);
+          resolve(app);
+        });
+      });
     });
   });
 };
@@ -131,11 +190,14 @@ module.exports = {
       console.log(req.file);
 
       App.create(app)
-        .then(buildDroplet)
         .then(buildDockerfile(req.file))
-        .then(buildAndRunContainer)
-        .then(saveIpAddress)
-        .catch(error => res.status(400).send(error));
+        .then(buildImage)
+        .then(createNetwork)
+        .then(createService)
+        .catch(error => {
+          console.log(error);
+          res.status(400).send(error)
+        });      
 
       return res.redirect(`/apps`)
     });
