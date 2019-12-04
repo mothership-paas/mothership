f98d7cad5efbb81a98d5bf7a833019d7cdccf54e
Ian Evans <icevans@icloud.com>
2019-12-04 16:43:11 -0700

    Add validations to user model

--- server/models/user.js
+++ server/models/user.js
@@ -3,8 +3,27 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
-    username: DataTypes.STRING,
-    password: DataTypes.STRING
+    username: {
+      type: DataTypes.STRING,
+      unique: { args: true, msg: 'Email is already registered', },
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'Please enter a valid email',
+        },
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: {
+          args: 5,
+          msg: 'Password must be between 5 and 50 characters',
+        }
+      }
+    }
   }, {});
   User.associate = function(models) {
     // associations can be defined here
