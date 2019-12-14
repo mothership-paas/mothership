ef229637ce464fde3ae96b9767cd56eafb8509a4
jkulton <jonkulton@gmail.com>
2019-12-13 20:53:11 -0500

    add initial imports for cluser controller

--- controllers/cluster.js
+++ controllers/cluster.js
@@ -1,3 +1,11 @@
+const DockerWrapper = require('../lib/DockerWrapper');
+const Node = require('../server/models').Node;
+const Database = require('../server/models').Database;
+const Config = require('../server/models').Config;
+const Machine = require('docker-machine');
+const uuid = require('uuid/v4');
+const errHandler = e => console.log(e);
+
 module.exports = {
 	show(req, res) {
     res.render('cluster');
