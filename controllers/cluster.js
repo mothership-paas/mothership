5d8a2c922e1c452d9ae434d71cd43abbd09feb0a
jkulton <jonkulton@gmail.com>
2019-12-13 20:54:19 -0500

    add endpoint to cluster controller for adding new node

--- controllers/cluster.js
+++ controllers/cluster.js
@@ -23,4 +23,43 @@ module.exports = {
         }
       });
   },
-}
+
+  async create(req, res) {
+    const nodes = await Node.findAll();
+    const workerNodes = nodes.filter(node => node.manager === false);
+    const managerNode = nodes.find(node => node.manager === true);
+    const allNodesActive = nodes.every(node => node.active);
+
+    if (!allNodesActive) {
+      const errors = [{ message: 'Node cannot be added while cluster is updating.' }];
+      return res.render('cluster/index', { errors, nodes });
+    }
+
+    const DO_TOKEN = ''; // TODO: send from API
+
+    const name = `worker-${uuid()}`;
+    const nodeParams = {
+      name,
+      manager: false,
+      active: false
+    };
+
+    console.log('Creating node in db...');
+    Node.create(nodeParams)
+    .then(() => res.redirect('/cluster'))
+    .then(DockerWrapper.createMachine(name, DO_TOKEN))
+    .then(DockerWrapper.joinSwarm(name))
+    .then(async () => {
+      const workerNode = await Node.findOne({ where: { name }});
+      const workerIp   = await DockerWrapper.getNodeIp(name).catch(errHandler);
+      console.log(`Updating db with IP address of worker node (${workerIp})...`);
+      const params = {
+        ip_address: workerIp,
+        active: true,
+      };
+      return workerNode.update(params);
+    })
+    .catch(errHandler);
+  },
+
+};
