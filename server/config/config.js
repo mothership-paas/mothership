76c0010ee587aa420e80b0ba3f10c7ee5a2b0e2a
Ian Evans <icevans@icloud.com>
2019-12-20 16:31:28 -0700

    Config test environment to use separate database
    Now the database name in the testing environment read from a
    second TEST_DB_NAME env variable (be sure to set this in your
    .env file). Since we typically want to modify the db during testing,
    we don't want to screw with your dev database.

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ module.exports = {
     port: 5432,
   },
   "test": {
-    database: process.env.DB_NAME,
+    database: process.env.TEST_DB_NAME,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
