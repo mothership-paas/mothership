f3a142c0d6cb682114cd6d1082936a3c28989f99
Bharat Agarwal <bharatagarwal@gmail.com>
2019-12-28 04:16:43 +0530

    Removing Sequelize import
    And removing `package.json` and `yarn.lock` additions from previous commit

--- controllers/cluster.js
+++ controllers/cluster.js
@@ -4,7 +4,6 @@ const Node = require('../server/models').Node;
 const Database = require('../server/models').Database;
 const Config = require('../server/models').Config;
 const Machine = require('docker-machine');
-const Sequelize = require('sequelize');
 const uuid = require('uuid/v4');
 const errHandler = e => console.log(e);
 
