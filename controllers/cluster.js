9e4bec9e82c93c5308c81ad31ff2f5dbfe67bfca
Bharat Agarwal <bharatagarwal@gmail.com>
2019-12-28 04:15:29 +0530

    Revert "Removing unnecessary sequelize import"
    This reverts commit 350d53220ab5ee7c34d090b5f29871f62eb86a2a.

--- controllers/cluster.js
+++ controllers/cluster.js
@@ -4,6 +4,7 @@ const Node = require('../server/models').Node;
 const Database = require('../server/models').Database;
 const Config = require('../server/models').Config;
 const Machine = require('docker-machine');
+const Sequelize = require('sequelize');
 const uuid = require('uuid/v4');
 const errHandler = e => console.log(e);
 
